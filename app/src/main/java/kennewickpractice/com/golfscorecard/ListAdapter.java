81765224fdee0c03578d6467e2a0737808ad5d36
Ken Wagner <kenwagner@hotmail.com>
2015-11-25 15:33:32 -0800

    created array and list adapter

---
+++ app/src/main/java/kennewickpractice/com/golfscorecard/ListAdapter.java
@@ -0,0 +1,31 @@
+package kennewickpractice.com.golfscorecard;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by ken.wagner on 11/25/2015.
+ */
+public class ListAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
